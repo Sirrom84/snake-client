ba23e218cc0237637b9c2e9909a5d17f43f75542
Rob <morrisrjc@gmail.com>
2021-01-14 21:27:22 +0000

    added constants and inputs

--- constants.js
+++ constants.js
@@ -1,7 +1,17 @@
 const IP = `135.23.222.131`;
 const PORT = 50542;
+const MOVE_UP_KEY = 'Move: up';
+const MOVE_DOWN_KEY = 'Move: down';
+const MOVE_LEFT_KEY = 'Move: left';
+const MOVE_RIGHT_KEY = 'Move: right';
+
+
 
 module.exports = {
   IP,
-  PORT
-}
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY
+};
